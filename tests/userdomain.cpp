42095f4359fcd080f5789788df02126e38ce8dc4
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2020-08-24 14:45:10 +0200

    add UserDomainCoordRange to iterator over coords of a UserDomain

--- tests/userdomain.cpp
+++ tests/userdomain.cpp
@@ -8,3 +8,52 @@ TEST_CASE("userdomain ctor")
     llama::UserDomain<1> ud{};
     CHECK(ud[0] == 0);
 }
+
+TEST_CASE("UserDomainCoordRange1D")
+{
+    llama::UserDomain<1> ud{3};
+
+    std::vector<llama::UserDomain<1>> coords;
+    for(auto coord : llama::UserDomainCoordRange{ud}) coords.push_back(coord);
+
+    CHECK(coords == std::vector<llama::UserDomain<1>>{{0}, {1}, {2}});
+}
+
+TEST_CASE("UserDomainCoordRange2D")
+{
+    llama::UserDomain<2> ud{3, 3};
+
+    std::vector<llama::UserDomain<2>> coords;
+    for(auto coord : llama::UserDomainCoordRange{ud}) coords.push_back(coord);
+
+    CHECK(
+        coords
+        == std::vector<llama::UserDomain<2>>{
+            {0, 0},
+            {0, 1},
+            {0, 2},
+            {1, 0},
+            {1, 1},
+            {1, 2},
+            {2, 0},
+            {2, 1},
+            {2, 2}});
+}
+
+TEST_CASE("UserDomainCoordRange3D")
+{
+    llama::UserDomain<3> ud{3, 3, 3};
+
+    std::vector<llama::UserDomain<3>> coords;
+    for(auto coord : llama::UserDomainCoordRange{ud}) coords.push_back(coord);
+
+    CHECK(
+        coords
+        == std::vector<llama::UserDomain<3>>{
+            {0, 0, 0}, {0, 0, 1}, {0, 0, 2}, {0, 1, 0}, {0, 1, 1}, {0, 1, 2},
+            {0, 2, 0}, {0, 2, 1}, {0, 2, 2}, {1, 0, 0}, {1, 0, 1}, {1, 0, 2},
+            {1, 1, 0}, {1, 1, 1}, {1, 1, 2}, {1, 2, 0}, {1, 2, 1}, {1, 2, 2},
+            {2, 0, 0}, {2, 0, 1}, {2, 0, 2}, {2, 1, 0}, {2, 1, 1}, {2, 1, 2},
+            {2, 2, 0}, {2, 2, 1}, {2, 2, 2},
+        });
+}
