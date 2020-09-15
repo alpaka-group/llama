e61d478c2213cef4ea9b9f786e67035b100195c4
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2020-09-15 10:03:54 +0200

    simplify AlignmentAllocator

--- include/llama/Allocators.hpp
+++ include/llama/Allocators.hpp
@@ -65,14 +65,6 @@ namespace llama::allocator
         struct AlignmentAllocator
         {
             using value_type = T;
-            using size_type = std::size_t;
-            using difference_type = std::ptrdiff_t;
-
-            using pointer = T *;
-            using const_pointer = T const *;
-
-            using reference = T &;
-            using const_reference = T const &;
 
             inline AlignmentAllocator() noexcept = default;
 
@@ -83,40 +75,15 @@ namespace llama::allocator
 
             inline ~AlignmentAllocator() noexcept = default;
 
-            inline auto adress(reference r) -> pointer
+            inline auto allocate(std::size_t n) -> T *
             {
-                return &r;
-            }
-            inline auto adress(const_reference r) const -> const_pointer
-            {
-                return &r;
+                return static_cast<T *>(::operator new[](
+                    n * sizeof(T), std::align_val_t{Alignment}));
             }
 
-            inline auto allocate(size_type n) -> pointer
-            {
-                return static_cast<pointer>(::operator new[](
-                    n * sizeof(value_type), std::align_val_t{Alignment}));
-            }
-
-            inline void deallocate(pointer p, size_type)
+            inline void deallocate(T * p, std::size_t)
             {
                 ::operator delete[](p, std::align_val_t{Alignment});
-            }
-
-            inline void construct(pointer p, value_type const &)
-            {
-                // commented out for performance reasons
-                // new ( p ) value_type ( value ); // FIXME this is a bug
-            }
-
-            inline auto destroy(pointer p) -> void
-            {
-                p->~value_type();
-            }
-
-            inline auto max_size() const noexcept -> size_type
-            {
-                return size_type(-1) / sizeof(value_type);
             }
 
             template<typename T2>
@@ -132,10 +99,6 @@ namespace llama::allocator
                 return !(*this == other);
             }
 
-            /* Returns true if and only if storage allocated from *this
-             * can be deallocated from other, and vice versa.
-             * Always returns true for stateless allocators.
-             */
             auto
             operator==(const AlignmentAllocator<T, Alignment> & other) const
                 -> bool
