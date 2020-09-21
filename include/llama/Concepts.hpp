c4465f999b706755c98139971291bfeb1c23596c
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2020-09-21 14:21:46 +0200

    guard Mapping concept by #ifdef __cpp_concepts

--- include/llama/Concepts.hpp
+++ include/llama/Concepts.hpp
@@ -1,11 +1,14 @@
 #pragma once
 
+#ifdef __cpp_concepts
+
 #include "Types.hpp"
 
 #include <concepts>
 
 namespace llama
 {
+    // clang-format off
     template <typename M>
     concept Mapping = requires(M m) {
         typename M::UserDomain;
@@ -14,4 +17,7 @@ namespace llama
         { m.getBlobSize(std::size_t{}) } -> std::convertible_to<std::size_t>;
         { m.getBlobNrAndOffset(typename M::UserDomain{}) } -> std::same_as<NrAndOffset>;
     };
+    // clang-format on
 }
+
+#endif
