9ed4960bb8a693b20e6e375e277c28fe08a6d18e
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2020-11-05 18:36:38 +0100

    add aosoa_copy and more benchmarks

--- examples/viewcopy/viewcopy.cpp
+++ examples/viewcopy/viewcopy.cpp
@@ -47,6 +47,78 @@ void naive_copy(const llama::View<Mapping1, BlobType1>& srcView, llama::View<Map
         });
 }
 
+template <
+    bool ReadOpt,
+    typename ArrayDomain,
+    typename DatumDomain,
+    std::size_t LanesSrc,
+    typename BlobType1,
+    std::size_t LanesDst,
+    typename BlobType2>
+void aosoa_copy(
+    const llama::View<
+        llama::mapping::AoSoA<ArrayDomain, DatumDomain, LanesSrc, llama::mapping::LinearizeArrayDomainCpp>,
+        BlobType1>& srcView,
+    llama::View<
+        llama::mapping::AoSoA<ArrayDomain, DatumDomain, LanesDst, llama::mapping::LinearizeArrayDomainCpp>,
+        BlobType2>& dstView)
+{
+    if (srcView.mapping.arrayDomainSize != dstView.mapping.arrayDomainSize)
+        throw std::runtime_error{"UserDomain sizes are different"};
+
+    const auto flatSize = std::reduce(
+        std::begin(dstView.mapping.arrayDomainSize),
+        std::end(dstView.mapping.arrayDomainSize),
+        std::size_t{1},
+        std::multiplies<>{});
+
+    const std::byte* src = srcView.storageBlobs[0].data();
+    std::byte* dst = dstView.storageBlobs[0].data();
+
+    // the same as AoSoA::getBlobNrAndOffset but takes a flat array index
+    auto map = [](std::size_t flatArrayIndex, auto coord, std::size_t Lanes) {
+        const auto blockIndex = flatArrayIndex / Lanes;
+        const auto laneIndex = flatArrayIndex % Lanes;
+        const auto offset = (llama::sizeOf<DatumDomain> * Lanes) * blockIndex
+            + llama::offsetOf<DatumDomain, decltype(coord)> * Lanes
+            + sizeof(llama::GetType<DatumDomain, decltype(coord)>) * laneIndex;
+        return offset;
+    };
+
+    if constexpr (ReadOpt)
+    {
+        // optimized for linear reading
+        for (std::size_t i = 0; i < flatSize; i += LanesSrc)
+        {
+            llama::forEach<DatumDomain>([&](auto coord) {
+                constexpr auto L = std::min(LanesSrc, LanesDst);
+                for (std::size_t j = 0; j < LanesSrc; j += L)
+                {
+                    constexpr auto bytes = L * sizeof(llama::GetType<DatumDomain, decltype(coord)>);
+                    std::memcpy(&dst[map(i + j, coord, LanesSrc)], src, bytes);
+                    src += bytes;
+                }
+            });
+        }
+    }
+    else
+    {
+        // optimized for linear writing
+        for (std::size_t i = 0; i < flatSize; i += LanesDst)
+        {
+            llama::forEach<DatumDomain>([&](auto coord) {
+                constexpr auto L = std::min(LanesSrc, LanesDst);
+                for (std::size_t j = 0; j < LanesDst; j += L)
+                {
+                    constexpr auto bytes = L * sizeof(llama::GetType<DatumDomain, decltype(coord)>);
+                    std::memcpy(dst, &src[map(i + j, coord, LanesSrc)], bytes);
+                    dst += bytes;
+                }
+            });
+        }
+    }
+}
+
 template <typename Mapping, typename BlobType>
 auto sumAllValues(const llama::View<Mapping, BlobType>& view)
 {
@@ -109,4 +181,50 @@ int main(int argc, char** argv)
             std::memcpy(view2.storageBlobs[0].data(), view1.storageBlobs[0].data(), view2.storageBlobs[0].size());
         });
     }
+
+    {
+        std::cout << "SoA -> AoS\n";
+        const auto srcMapping = llama::mapping::SoA{userDomain, Particle{}};
+        const auto dstMapping = llama::mapping::AoS{userDomain, Particle{}};
+
+        auto [srcView, srcCheckSum] = prepareViewAndChecksum(srcMapping);
+        benchmarkCopy("naive_copy", srcView, srcCheckSum, dstMapping, [](const auto& view1, auto& view2) {
+            naive_copy(view1, view2);
+        });
+        benchmarkCopy("memcpy    ", srcView, srcCheckSum, dstMapping, [](const auto& view1, auto& view2) {
+            static_assert(view1.storageBlobs.rank == 1);
+            static_assert(view2.storageBlobs.rank == 1);
+            std::memcpy(view2.storageBlobs[0].data(), view1.storageBlobs[0].data(), view2.storageBlobs[0].size());
+        });
+    }
+
+    using namespace boost::mp11;
+    mp_for_each<mp_list<
+        mp_list_c<std::size_t, 8, 32>,
+        mp_list_c<std::size_t, 8, 64>,
+        mp_list_c<std::size_t, 32, 8>,
+        mp_list_c<std::size_t, 64, 8>>>([&](auto pair) {
+        constexpr auto LanesSrc = mp_first<decltype(pair)>::value;
+        constexpr auto LanesDst = mp_second<decltype(pair)>::value;
+
+        std::cout << "AoSoA" << LanesSrc << " -> AoSoA" << LanesDst << "\n"; // e.g. AVX2 -> CUDA
+        const auto srcMapping = llama::mapping::AoSoA<decltype(userDomain), Particle, LanesSrc>{userDomain};
+        const auto dstMapping = llama::mapping::AoSoA<decltype(userDomain), Particle, LanesDst>{userDomain};
+
+        auto [srcView, srcCheckSum] = prepareViewAndChecksum(srcMapping);
+        benchmarkCopy("naive_copy   ", srcView, srcCheckSum, dstMapping, [](const auto& view1, auto& view2) {
+            naive_copy(view1, view2);
+        });
+        benchmarkCopy("memcpy       ", srcView, srcCheckSum, dstMapping, [](const auto& view1, auto& view2) {
+            static_assert(view1.storageBlobs.rank == 1);
+            static_assert(view2.storageBlobs.rank == 1);
+            std::memcpy(view2.storageBlobs[0].data(), view1.storageBlobs[0].data(), view2.storageBlobs[0].size());
+        });
+        benchmarkCopy("aosoa_copy(r)", srcView, srcCheckSum, dstMapping, [](const auto& view1, auto& view2) {
+            aosoa_copy<true>(view1, view2);
+        });
+        benchmarkCopy("aosoa_copy(w)", srcView, srcCheckSum, dstMapping, [](const auto& view1, auto& view2) {
+            aosoa_copy<false>(view1, view2);
+        });
+    });
 }
