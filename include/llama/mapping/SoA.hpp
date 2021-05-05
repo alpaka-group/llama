6dbb5b68e855a560e1bee01b2cca0a48fb219b05
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2021-05-05 11:31:20 +0200

    simplify SoA blob selection

--- include/llama/mapping/SoA.hpp
+++ include/llama/mapping/SoA.hpp
@@ -23,7 +23,7 @@ namespace llama::mapping
         using ArrayDims = T_ArrayDims;
         using RecordDim = T_RecordDim;
         static constexpr std::size_t blobCount
-            = SeparateBuffers ? boost::mp11::mp_size<llama::FlattenRecordDim<RecordDim>>::value : 1;
+            = SeparateBuffers ? boost::mp11::mp_size<FlattenRecordDim<RecordDim>>::value : 1;
 
         constexpr SoA() = default;
 
@@ -42,11 +42,10 @@ namespace llama::mapping
         {
             if constexpr (SeparateBuffers)
             {
-                constexpr llama::Array<std::size_t, blobCount> typeSizes = []() constexpr
+                constexpr Array<std::size_t, blobCount> typeSizes = []() constexpr
                 {
-                    llama::Array<std::size_t, blobCount> r{};
-                    std::size_t i = 0;
-                    forEachLeaf<RecordDim>([&](auto coord) constexpr
+                    Array<std::size_t, blobCount> r{};
+                    forEachLeaf<RecordDim>([&r, i = 0](auto coord) mutable constexpr
                                            { r[i++] = sizeof(GetType<RecordDim, decltype(coord)>); });
                     return r;
                 }
@@ -54,9 +53,7 @@ namespace llama::mapping
                 return LinearizeArrayDimsFunctor{}.size(arrayDimsSize) * typeSizes[blobIndex];
             }
             else
-            {
                 return LinearizeArrayDimsFunctor{}.size(arrayDimsSize) * sizeOf<RecordDim>;
-            }
         }
 
         template <std::size_t... RecordCoords>
@@ -64,27 +61,7 @@ namespace llama::mapping
         {
             if constexpr (SeparateBuffers)
             {
-                using TargetRecordCoord = RecordCoord<RecordCoords...>;
-                constexpr auto blob = [&]() constexpr
-                {
-                    std::size_t index = 0;
-                    bool found = false;
-                    forEachLeaf<RecordDim>([&](auto c) constexpr
-                                           {
-                                               if constexpr (std::is_same_v<decltype(c), TargetRecordCoord>)
-                                                   found = true;
-                                               else if (!found)
-                                                   index++;
-                                           });
-                    if (!found)
-                        return std::numeric_limits<std::size_t>::max();
-                    return index;
-                }
-                ();
-                static_assert(
-                    blob != std::numeric_limits<std::size_t>::max(),
-                    "Passed TargetRecordCoord must be in record dimension");
-
+                constexpr auto blob = flatRecordCoord<RecordDim, RecordCoord<RecordCoords...>>;
                 const auto offset = LinearizeArrayDimsFunctor{}(coord, arrayDimsSize)
                     * sizeof(GetType<RecordDim, RecordCoord<RecordCoords...>>);
                 return {blob, offset};
