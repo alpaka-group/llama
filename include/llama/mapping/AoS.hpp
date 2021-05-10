e60abe39db74fa8bc76590893c3bae54748414cd
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2021-05-10 19:13:48 +0200

    allow AoS to minimize padding
    Allow customizing how the record dimension is flatten for AoS.
    Add FlattenRecordDimMinimizePadding flattening strategy.
    Add FlattenRecordDimInOrder flattening strategy.

--- include/llama/mapping/AoS.hpp
+++ include/llama/mapping/AoS.hpp
@@ -9,14 +9,17 @@ namespace llama::mapping
 {
     /// Array of struct mapping. Used to create a \ref View via \ref allocView.
     /// \tparam AlignAndPad If true, padding bytes are inserted to guarantee that struct members are properly aligned.
-    /// If false, struct members are tighly packed.
+    /// If false, struct members are tightly packed.
     /// \tparam LinearizeArrayDimsFunctor Defines how the array dimensions should be mapped into linear numbers and
     /// how big the linear domain gets.
+    /// \tparam FlattenRecordDim Defines how the record dimension's fields should be flattended. See \ref
+    /// FlattenRecordDimInOrder and \ref FlattenRecordDimMinimizePadding.
     template <
         typename T_ArrayDims,
         typename T_RecordDim,
         bool AlignAndPad = true,
-        typename LinearizeArrayDimsFunctor = LinearizeArrayDimsCpp>
+        typename LinearizeArrayDimsFunctor = LinearizeArrayDimsCpp,
+        template <typename> typename FlattenRecordDim = FlattenRecordDimInOrder>
     struct AoS
     {
         using ArrayDims = T_ArrayDims;
@@ -37,27 +40,41 @@ namespace llama::mapping
 
         LLAMA_FN_HOST_ACC_INLINE constexpr auto blobSize(std::size_t) const -> std::size_t
         {
-            return LinearizeArrayDimsFunctor{}.size(arrayDimsSize) * sizeOf<RecordDim, AlignAndPad>;
+            return LinearizeArrayDimsFunctor{}.size(arrayDimsSize)
+                * flatSizeOf<typename Flattener::FlatRecordDim, AlignAndPad>;
         }
 
         template <std::size_t... RecordCoords>
         LLAMA_FN_HOST_ACC_INLINE constexpr auto blobNrAndOffset(ArrayDims coord) const -> NrAndOffset
         {
+            constexpr std::size_t flatIndex =
+#ifdef __NVCC__
+                Flattener{}.template flatIndex<RecordCoords...>;
+#else
+                Flattener::template flatIndex<RecordCoords...>;
+#endif
             const auto offset = LinearizeArrayDimsFunctor{}(coord, arrayDimsSize)
-                    * sizeOf<RecordDim, AlignAndPad> + offsetOf<RecordDim, RecordCoord<RecordCoords...>, AlignAndPad>;
+                    * flatSizeOf<typename Flattener::FlatRecordDim,
+                                 AlignAndPad> + flatOffsetOf<typename Flattener::FlatRecordDim, flatIndex, AlignAndPad>;
             return {0, offset};
         }
 
     private:
+        using Flattener = FlattenRecordDim<T_RecordDim>;
         ArrayDims arrayDimsSize;
     };
 
-    /// Array of struct mapping preserving the alignment of the element types by inserting padding.
+    /// Array of struct mapping preserving the alignment of the field types by inserting padding.
     /// \see AoS
     template <typename ArrayDims, typename RecordDim, typename LinearizeArrayDimsFunctor = LinearizeArrayDimsCpp>
     using AlignedAoS = AoS<ArrayDims, RecordDim, true, LinearizeArrayDimsFunctor>;
 
-    /// Array of struct mapping packing the element types tighly, violating the types alignment requirements.
+    /// Array of struct mapping preserving the alignment of the field types by inserting padding and permuting the field
+    /// order to minimize this padding. \see AoS
+    template <typename ArrayDims, typename RecordDim, typename LinearizeArrayDimsFunctor = LinearizeArrayDimsCpp>
+    using MinAlignedAoS = AoS<ArrayDims, RecordDim, true, LinearizeArrayDimsFunctor, FlattenRecordDimMinimizePadding>;
+
+    /// Array of struct mapping packing the field types tightly, violating the types alignment requirements.
     /// \see AoS
     template <typename ArrayDims, typename RecordDim, typename LinearizeArrayDimsFunctor = LinearizeArrayDimsCpp>
     using PackedAoS = AoS<ArrayDims, RecordDim, false, LinearizeArrayDimsFunctor>;
