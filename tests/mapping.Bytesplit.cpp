f6ec077ff5a05eda748e2c0544597eb763347598
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2021-10-11 19:26:04 +0200

    add a test to verify byte layout of Bytesplit mapping

--- tests/mapping.Bytesplit.cpp
+++ tests/mapping.Bytesplit.cpp
@@ -1,5 +1,7 @@
 #include "common.hpp"
 
+//#include <bit>
+
 TEST_CASE("mapping.ByteSplit.AoS")
 {
     auto view = llama::allocView(
@@ -25,4 +27,25 @@ TEST_CASE("mapping.ByteSplit.AoSoA")
             {128}});
     iotaFillView(view);
     iotaCheckView(view);
+}
+
+TEST_CASE("mapping.ByteSplit.SoA.verify")
+{
+    using Mapping
+        = llama::mapping::Bytesplit<llama::ArrayExtentsDynamic<1>, Vec3I, llama::mapping::PreconfiguredSoA<>::type>;
+    auto view = llama::allocView(Mapping{{128}});
+    for(auto i = 0; i < 128; i++)
+        view(i) = i;
+
+    CHECK(Mapping::blobCount == 12);
+
+    for(auto b = 0; b < 12; b++)
+    {
+        // TODO(bgruber): assumes little endian for now. Upgrade to C++20 and enable.
+        // const auto isNonZero = std::endian::native == std::endian::little ? (b == 0 || b == 4 || b == 8)
+        //                                                                  : (b == 3 || b == 7 || b == 11);
+        const auto isNonZero = (b == 0 || b == 4 || b == 8);
+        for(auto i = 0; i < 128; i++)
+            CHECK(view.storageBlobs[b][i] == (isNonZero ? static_cast<std::byte>(i) : std::byte{0}));
+    }
 }
