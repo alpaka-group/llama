ef045ec2022533bb5c40f5e3c9cb7bd8a3b8fa86
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2021-12-14 15:55:22 +0100

    support computed mappings inside Bytesplit

--- tests/mapping.Bytesplit.cpp
+++ tests/mapping.Bytesplit.cpp
@@ -29,6 +29,34 @@ TEST_CASE("mapping.ByteSplit.AoSoA")
     iotaCheckView(view);
 }
 
+TEST_CASE("mapping.ByteSplit.ChangeType.SoA")
+{
+    using Mapping = llama::mapping::Bytesplit<
+        llama::ArrayExtentsDynamic<1>,
+        Vec3I,
+        llama::mapping::PreconfiguredChangeType<
+            llama::mapping::PreconfiguredSoA<>::type,
+            boost::mp11::mp_list<boost::mp11::mp_list<std::byte, unsigned char>>>::type>;
+
+    STATIC_REQUIRE(std::is_same_v<Mapping::RecordDim, Vec3I>);
+    STATIC_REQUIRE(std::is_same_v<
+                   typename Mapping::Inner::RecordDim,
+                   llama::Record<
+                       llama::Field<tag::X, std::byte[4]>,
+                       llama::Field<tag::Y, std::byte[4]>,
+                       llama::Field<tag::Z, std::byte[4]>>>);
+    STATIC_REQUIRE(std::is_same_v<
+                   typename Mapping::Inner::Inner::RecordDim,
+                   llama::Record<
+                       llama::Field<tag::X, unsigned char[4]>,
+                       llama::Field<tag::Y, unsigned char[4]>,
+                       llama::Field<tag::Z, unsigned char[4]>>>);
+
+    auto view = llama::allocView(Mapping{{128}});
+    iotaFillView(view);
+    iotaCheckView(view);
+}
+
 TEST_CASE("mapping.ByteSplit.SoA.verify")
 {
     using Mapping
