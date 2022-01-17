c83543c525823561862d5c437aff1cc46f6aec84
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2022-01-17 14:31:46 +0100

    bump version to 0.4

--- docs/conf.py
+++ docs/conf.py
@@ -40,9 +40,9 @@ copyright = u'2018, Alexander Matthes, 2021, Bernhard Manfred Gruber'
 author = u'Alexander Matthes, Bernhard Manfred Gruber'
 
 # The short X.Y version
-version = u'0.3'
+version = u'0.4'
 # The full version, including alpha/beta/rc tags
-release = u'0.3'
+release = u'0.4'
 
 
 # -- General configuration ---------------------------------------------------
