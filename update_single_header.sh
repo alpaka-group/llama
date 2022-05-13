e949a40f6c0b41755935d8f7e9f606eb31a66c63
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2022-05-13 15:27:54 +0200

    fix mkdir not failing if directory exists

--- update_single_header.sh
+++ update_single_header.sh
@@ -7,6 +7,6 @@ git clone https://github.com/shrpnsld/amalgamate.git --depth 1 $amalgamate_tmp_d
 cd include/llama
 $amalgamate_tmp_dir/clone/amalgamate -o $amalgamate_tmp_dir -H -v -a -n 'llama'
 cd ../..
-mkdir $destination_dir
+mkdir -p $destination_dir
 mv $amalgamate_tmp_dir/llama-amalgamated/llama.hpp $destination_dir
 rm -rf $amalgamate_tmp_dir
