43b1f36d071615f99afbfe11c22a45798bae068b
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2022-06-17 15:27:59 +0200

    Fix uploading single-header llama.hpp

--- tools/create-single-header.sh
+++ tools/create-single-header.sh
@@ -2,11 +2,12 @@
 
 workingcopy_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )/.."
 amalgamate_tmp_dir=$workingcopy_dir/_amalgamate_tmp
-destination_dir=$workingcopy_dir/single_header
+destination_dir=$workingcopy_dir/single-header
 
 git clone https://github.com/shrpnsld/amalgamate.git --depth 1 $amalgamate_tmp_dir/clone
 cd include/llama
 $amalgamate_tmp_dir/clone/amalgamate -o $amalgamate_tmp_dir -H -v -a -n 'llama'
 cd ../..
-mv $amalgamate_tmp_dir/llama-amalgamated/llama.hpp $workingcopy_dir
+mkdir -p $destination_dir
+mv $amalgamate_tmp_dir/llama-amalgamated/llama.hpp $destination_dir
 rm -rf $amalgamate_tmp_dir
