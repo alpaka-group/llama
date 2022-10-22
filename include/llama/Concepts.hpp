404a73ad0017537e2211b191f50a77928ecbe344
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2022-10-22 13:27:28 +0200

    Improve readability of constexpr check in mapping concept

--- include/llama/Concepts.hpp
+++ include/llama/Concepts.hpp
@@ -20,7 +20,7 @@ namespace llama
         typename M::RecordDim;
         { m.extents() } -> std::same_as<typename M::ArrayExtents>;
         { +M::blobCount } -> std::same_as<std::size_t>;
-        Array<int, M::blobCount>{}; // validates constexpr-ness
+        std::integral_constant<std::size_t, M::blobCount>{}; // validates constexpr-ness
         { m.blobSize(typename M::ArrayExtents::value_type{}) } -> std::same_as<typename M::ArrayExtents::value_type>;
     };
 
