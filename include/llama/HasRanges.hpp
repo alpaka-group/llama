cdb5b93fd7b35d740f1adbe812170247d0f15cdd
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2022-10-31 02:45:21 +0100

    Add clang-15 to CI

--- include/llama/HasRanges.hpp
+++ include/llama/HasRanges.hpp
@@ -2,13 +2,14 @@
 
 #pragma once
 
-// TODO(bgruber): clang 10-14 (libstdc++ from gcc 11.2) fail to compile this currently with the issue described here:
+// TODO(bgruber): clang 10-15 (libstdc++ from gcc 11.2 or gcc 12.1) fail to compile this currently with the issue
+// described here:
 // https://stackoverflow.com/questions/64300832/why-does-clang-think-gccs-subrange-does-not-satisfy-gccs-ranges-begin-functi
 // Intel LLVM compiler is also using the clang frontend
 #define CAN_USE_RANGES 0
 #if __has_include(<version>)
 #    include <version>
-#    if defined(__cpp_concepts) && defined(__cpp_lib_ranges) && (!defined(__clang__) || __clang_major__ >= 15)        \
+#    if defined(__cpp_concepts) && defined(__cpp_lib_ranges) && (!defined(__clang__) || __clang_major__ >= 16)        \
         && !defined(__INTEL_LLVM_COMPILER) && (!defined(_MSC_VER) || _MSC_VER > 1932) && !defined(__NVCOMPILER)
 #        undef CAN_USE_RANGES
 #        define CAN_USE_RANGES 1
