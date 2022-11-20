415203184acc26d2dbcca00aa35af7169a698e7d
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2022-11-20 18:25:56 +0100

    Extend PermuteArrayIndex constructor overload set
    Additionally allows construction from inner mapping

--- tests/mapping.PermuteArrayIndex.cpp
+++ tests/mapping.PermuteArrayIndex.cpp
@@ -1,9 +1,21 @@
 #include "common.hpp"
 
+TEST_CASE("mapping.PermuteArrayIndex.ctors")
+{
+    using AE = llama::ArrayExtentsDynamic<int, 3>;
+    using InnerMapping = llama::mapping::AoS<AE, Vec3I>;
+
+    auto inner = InnerMapping{AE{3, 4, 5}};
+    [[maybe_unused]] auto perm1 = llama::mapping::PermuteArrayIndex<InnerMapping, 2, 0, 1>{};
+    [[maybe_unused]] auto perm2 = llama::mapping::PermuteArrayIndex<InnerMapping, 2, 0, 1>{AE{3, 4, 5}};
+    [[maybe_unused]] auto perm3 = llama::mapping::PermuteArrayIndex<InnerMapping, 2, 0, 1>{inner};
+}
+
 TEST_CASE("mapping.PermuteArrayIndex")
 {
     using InnerMapping = llama::mapping::AoS<llama::ArrayExtents<int, 4, 5, 6>, Vec3I>;
-    auto view = llama::allocView(InnerMapping{{}});
+    auto inner = InnerMapping{{}};
+    auto view = llama::allocView(inner);
 
     for(int x = 0; x < 4; x++)
         for(int y = 0; y < 5; y++)
