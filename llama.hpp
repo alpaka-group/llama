50c151f2c4ff9dec2c67c58780a4e2a61ea1d189
GitHub Action <action@github.com>
2023-01-16 17:39:21 +0000

    Update llama.hpp

--- llama.hpp
+++ llama.hpp
@@ -5947,7 +5947,7 @@ namespace llama
 // ============================================================================
 
 // ============================================================================
-// == ./mapping/BitPackedIntSoA.hpp ==
+// == ./mapping/BitPackedInt.hpp ==
 // ==
 // SPDX-License-Identifier: GPL-3.0-or-later
 
@@ -6169,6 +6169,118 @@ namespace llama::mapping
         template<typename RecordDim>
         using StoredUnsignedFor = std::
             conditional_t<(sizeof(LargestIntegral<RecordDim>) > sizeof(std::uint32_t)), std::uint64_t, std::uint32_t>;
+
+        template<
+            typename TArrayExtents,
+            typename TRecordDim,
+            typename Bits,
+            SignBit SignBit,
+            typename TLinearizeArrayDimsFunctor,
+            typename TStoredIntegral>
+        struct BitPackedIntCommon
+            : MappingBase<TArrayExtents, TRecordDim>
+            , protected llama::internal::BoxedValue<Bits>
+        {
+            using LinearizeArrayDimsFunctor = TLinearizeArrayDimsFunctor;
+            using StoredIntegral = TStoredIntegral;
+            static constexpr std::size_t blobCount = mp_size<FlatRecordDim<TRecordDim>>::value;
+
+            static_assert(std::is_integral_v<StoredIntegral>);
+            static_assert(std::is_unsigned_v<StoredIntegral>);
+
+            // We could allow more integer types as storage type, but that needs to be thought through carefully
+            static_assert(
+                std::is_same_v<StoredIntegral, std::uint32_t> || std::is_same_v<StoredIntegral, std::uint64_t>);
+
+        protected:
+            using Base = MappingBase<TArrayExtents, TRecordDim>;
+            using VHBits = llama::internal::BoxedValue<Bits>;
+            using size_type = typename TArrayExtents::value_type;
+
+            template<typename T>
+            using IsAllowedFieldType = mp_or<std::is_integral<T>, std::is_enum<T>>;
+
+            static_assert(
+                mp_all_of<FlatRecordDim<TRecordDim>, IsAllowedFieldType>::value,
+                "All record dimension field types must be integral");
+
+            template<typename T>
+            using IsFieldTypeSmallerOrEqualStorageIntegral = mp_bool<sizeof(T) <= sizeof(StoredIntegral)>;
+
+            static_assert(
+                mp_all_of<FlatRecordDim<TRecordDim>, IsFieldTypeSmallerOrEqualStorageIntegral>::value,
+                "The integral type used for storage must be at least as big as the type of the values to retrieve");
+
+        public:
+            LLAMA_FN_HOST_ACC_INLINE
+            constexpr auto bits() const -> size_type
+            {
+                return static_cast<size_type>(VHBits::value());
+            }
+
+            template<typename B = Bits, std::enable_if_t<isConstant<B>, int> = 0>
+            LLAMA_FN_HOST_ACC_INLINE constexpr explicit BitPackedIntCommon(
+                TArrayExtents extents = {},
+                Bits bits = {},
+                TRecordDim = {})
+                : Base(extents)
+                , VHBits{bits}
+            {
+                static_assert(VHBits::value() > 0);
+                mp_for_each<mp_transform<mp_identity, FlatRecordDim<TRecordDim>>>(
+                    [&](auto t)
+                    {
+                        using FieldType = typename decltype(t)::type;
+                        static_assert(
+                            static_cast<std::size_t>(VHBits::value()) <= sizeof(FieldType) * CHAR_BIT,
+                            "Storage bits must not be greater than bits of field type");
+                        static_assert(
+                            VHBits::value() >= 2
+                                || std::is_unsigned_v<FieldType> || SignBit == llama::mapping::SignBit::Discard,
+                            "When keeping the sign bit, Bits must be at least 2 with signed integers in the record "
+                            "dimension");
+                    });
+            }
+
+            template<typename B = Bits, std::enable_if_t<!isConstant<B>, int> = 0>
+            LLAMA_FN_HOST_ACC_INLINE constexpr explicit BitPackedIntCommon(
+                TArrayExtents extents,
+                Bits bits,
+                TRecordDim = {})
+                : Base(extents)
+                , VHBits{bits}
+            {
+#ifdef __CUDA_ARCH__
+                assert(VHBits::value() > 0);
+#else
+                if(VHBits::value() <= 0)
+                    throw std::invalid_argument("BitPackedInt* Bits must not be zero");
+#endif
+                mp_for_each<mp_transform<mp_identity, FlatRecordDim<TRecordDim>>>(
+                    [&](auto t)
+                    {
+                        using FieldType = typename decltype(t)::type;
+#ifdef __CUDA_ARCH__
+                        assert(VHBits::value() <= sizeof(FieldType) * CHAR_BIT);
+#else
+                        if(static_cast<std::size_t>(VHBits::value()) > sizeof(FieldType) * CHAR_BIT)
+                            throw std::invalid_argument(
+                                "BitPackedInt* Bits must not be larger than any field type in the record dimension");
+                        if(!(VHBits::value() >= 2
+                             || std::is_unsigned_v<FieldType> || SignBit == llama::mapping::SignBit::Discard))
+                            throw std::invalid_argument("When keeping the sign bit, Bits must be at least 2 with "
+                                                        "signed integers in the record "
+                                                        "dimension");
+#endif
+                    });
+            }
+
+            template<std::size_t... RecordCoords>
+            static constexpr auto isComputed(RecordCoord<RecordCoords...>)
+            {
+                return true;
+            }
+        };
     } // namespace internal
 
     /// Struct of array mapping using bit packing to reduce size/precision of integral data types. If your record
@@ -6190,111 +6302,24 @@ namespace llama::mapping
         typename TLinearizeArrayDimsFunctor = LinearizeArrayDimsCpp,
         typename TStoredIntegral = internal::StoredUnsignedFor<TRecordDim>>
     struct BitPackedIntSoA
-        : MappingBase<TArrayExtents, TRecordDim>
-        , private llama::internal::BoxedValue<Bits>
+        : internal::
+              BitPackedIntCommon<TArrayExtents, TRecordDim, Bits, SignBit, TLinearizeArrayDimsFunctor, TStoredIntegral>
     {
-        using LinearizeArrayDimsFunctor = TLinearizeArrayDimsFunctor;
-        using StoredIntegral = TStoredIntegral;
-        static constexpr std::size_t blobCount = mp_size<FlatRecordDim<TRecordDim>>::value;
-
-        static_assert(std::is_integral_v<StoredIntegral>);
-        static_assert(std::is_unsigned_v<StoredIntegral>);
-
-        // We could allow more integer types as storage type, but that needs to be thought through carefully
-        static_assert(std::is_same_v<StoredIntegral, std::uint32_t> || std::is_same_v<StoredIntegral, std::uint64_t>);
-
     private:
-        using Base = MappingBase<TArrayExtents, TRecordDim>;
-        using VHBits = llama::internal::BoxedValue<Bits>;
-        using size_type = typename TArrayExtents::value_type;
-
-        template<typename T>
-        using IsAllowedFieldType = mp_or<std::is_integral<T>, std::is_enum<T>>;
-
-        static_assert(
-            mp_all_of<FlatRecordDim<TRecordDim>, IsAllowedFieldType>::value,
-            "All record dimension field types must be integral");
-
-        template<typename T>
-        using IsFieldTypeSmallerOrEqualStorageIntegral = mp_bool<sizeof(T) <= sizeof(StoredIntegral)>;
-
-        static_assert(
-            mp_all_of<FlatRecordDim<TRecordDim>, IsFieldTypeSmallerOrEqualStorageIntegral>::value,
-            "The integral type used for storage must be at least as big as the type of the values to retrieve");
+        using Base = internal::
+            BitPackedIntCommon<TArrayExtents, TRecordDim, Bits, SignBit, TLinearizeArrayDimsFunctor, TStoredIntegral>;
 
     public:
-        LLAMA_FN_HOST_ACC_INLINE
-        constexpr auto bits() const -> size_type
-        {
-            return static_cast<size_type>(VHBits::value());
-        }
-
-        template<typename B = Bits, std::enable_if_t<isConstant<B>, int> = 0>
-        LLAMA_FN_HOST_ACC_INLINE constexpr explicit BitPackedIntSoA(
-            TArrayExtents extents = {},
-            Bits bits = {},
-            TRecordDim = {})
-            : Base(extents)
-            , VHBits{bits}
-        {
-            static_assert(VHBits::value() > 0);
-            mp_for_each<mp_transform<mp_identity, FlatRecordDim<TRecordDim>>>(
-                [&](auto t)
-                {
-                    using FieldType = typename decltype(t)::type;
-                    static_assert(
-                        static_cast<std::size_t>(VHBits::value()) <= sizeof(FieldType) * CHAR_BIT,
-                        "Storage bits must not be greater than bits of field type");
-                    static_assert(
-                        VHBits::value() >= 2
-                            || std::is_unsigned_v<FieldType> || SignBit == llama::mapping::SignBit::Discard,
-                        "When keeping the sign bit, Bits must be at least 2 with signed integers in the record "
-                        "dimension");
-                });
-        }
-
-        template<typename B = Bits, std::enable_if_t<!isConstant<B>, int> = 0>
-        LLAMA_FN_HOST_ACC_INLINE constexpr explicit BitPackedIntSoA(TArrayExtents extents, Bits bits, TRecordDim = {})
-            : Base(extents)
-            , VHBits{bits}
-        {
-#ifdef __CUDA_ARCH__
-            assert(VHBits::value() > 0);
-#else
-            if(VHBits::value() <= 0)
-                throw std::invalid_argument("BitPackedIntSoA Bits must not be zero");
-#endif
-            mp_for_each<mp_transform<mp_identity, FlatRecordDim<TRecordDim>>>(
-                [&](auto t)
-                {
-                    using FieldType = typename decltype(t)::type;
-#ifdef __CUDA_ARCH__
-                    assert(VHBits::value() <= sizeof(FieldType) * CHAR_BIT);
-#else
-                    if(static_cast<std::size_t>(VHBits::value()) > sizeof(FieldType) * CHAR_BIT)
-                        throw std::invalid_argument(
-                            "BitPackedIntSoA Bits must not be larger than any field type in the record dimension");
-                    if(!(VHBits::value() >= 2
-                         || std::is_unsigned_v<FieldType> || SignBit == llama::mapping::SignBit::Discard))
-                        throw std::invalid_argument(
-                            "When keeping the sign bit, Bits must be at least 2 with signed integers in the record "
-                            "dimension");
-#endif
-                });
-        }
+        using Base::Base;
+        using typename Base::size_type;
+        using VHBits = typename Base::VHBits; // use plain using declaration with nvcc >= 11.8
 
         LLAMA_FN_HOST_ACC_INLINE
         constexpr auto blobSize(size_type /*blobIndex*/) const -> size_type
         {
-            constexpr auto bitsPerStoredIntegral = static_cast<size_type>(sizeof(StoredIntegral) * CHAR_BIT);
-            const auto bitsNeeded = LinearizeArrayDimsFunctor{}.size(Base::extents()) * VHBits::value();
+            constexpr auto bitsPerStoredIntegral = static_cast<size_type>(sizeof(TStoredIntegral) * CHAR_BIT);
+            const auto bitsNeeded = TLinearizeArrayDimsFunctor{}.size(Base::extents()) * VHBits::value();
             return roundUpToMultiple(bitsNeeded, bitsPerStoredIntegral) / CHAR_BIT;
-        }
-
-        template<std::size_t... RecordCoords>
-        static constexpr auto isComputed(RecordCoord<RecordCoords...>)
-        {
-            return true;
         }
 
         template<std::size_t... RecordCoords, typename Blobs>
@@ -6304,9 +6329,9 @@ namespace llama::mapping
             Blobs& blobs) const
         {
             constexpr auto blob = flatRecordCoord<TRecordDim, RecordCoord<RecordCoords...>>;
-            const auto bitOffset = LinearizeArrayDimsFunctor{}(ai, Base::extents()) * VHBits::value();
+            const auto bitOffset = TLinearizeArrayDimsFunctor{}(ai, Base::extents()) * VHBits::value();
 
-            using QualifiedStoredIntegral = CopyConst<Blobs, StoredIntegral>;
+            using QualifiedStoredIntegral = CopyConst<Blobs, TStoredIntegral>;
             using DstType = GetType<TRecordDim, RecordCoord<RecordCoords...>>;
             LLAMA_BEGIN_SUPPRESS_HOST_DEVICE_WARNING
             return internal::BitPackedIntRef<DstType, QualifiedStoredIntegral, VHBits, size_type, SignBit>{
@@ -6351,9 +6376,122 @@ namespace llama::mapping
         typename StoredIntegral>
     inline constexpr bool isBitPackedIntSoA<
         BitPackedIntSoA<ArrayExtents, RecordDim, Bits, SignBit, LinearizeArrayDimsFunctor, StoredIntegral>> = true;
+
+    /// Array of struct mapping using bit packing to reduce size/precision of integral data types. If your record
+    /// dimension contains non-integral types, split them off using the \ref Split mapping first.
+    /// \tparam Bits If Bits is llama::Constant<N>, the compile-time N specifies the number of bits to use. If Bits is
+    /// an integral type T, the number of bits is specified at runtime, passed to the constructor and stored as type T.
+    /// Must not be zero and must not be bigger than the bits of TStoredIntegral.
+    /// @tparam SignBit When set to SignBit::Discard, discards the sign bit when storing signed integers. All
+    /// numbers will be read back positive.
+    /// \tparam TLinearizeArrayDimsFunctor Defines how the array dimensions should be mapped into linear numbers and
+    /// how big the linear domain gets.
+    /// \tparam FlattenRecordDim Defines how the record dimension's fields should be flattened. See \ref
+    //  FlattenRecordDimInOrder, \ref FlattenRecordDimIncreasingAlignment, \ref FlattenRecordDimDecreasingAlignment and
+    //  \ref FlattenRecordDimMinimizePadding.
+    /// \tparam TStoredIntegral Integral type used as storage of reduced precision integers. Must be std::uint32_t or
+    /// std::uint64_t.
+    template<
+        typename TArrayExtents,
+        typename TRecordDim,
+        typename Bits = typename TArrayExtents::value_type,
+        SignBit SignBit = SignBit::Keep,
+        typename TLinearizeArrayDimsFunctor = LinearizeArrayDimsCpp,
+        template<typename> typename FlattenRecordDim = FlattenRecordDimInOrder,
+        typename TStoredIntegral = internal::StoredUnsignedFor<TRecordDim>>
+    struct BitPackedIntAoS
+        : internal::
+              BitPackedIntCommon<TArrayExtents, TRecordDim, Bits, SignBit, TLinearizeArrayDimsFunctor, TStoredIntegral>
+    {
+    private:
+        using Base = internal::
+            BitPackedIntCommon<TArrayExtents, TRecordDim, Bits, SignBit, TLinearizeArrayDimsFunctor, TStoredIntegral>;
+
+    public:
+        using Base::Base;
+        using typename Base::size_type;
+        using VHBits = typename Base::VHBits; // use plain using declaration with nvcc >= 11.8
+
+        using Flattener = FlattenRecordDim<TRecordDim>;
+
+        LLAMA_FN_HOST_ACC_INLINE
+        constexpr auto blobSize(size_type /*blobIndex*/) const -> size_type
+        {
+            constexpr auto bitsPerStoredIntegral = static_cast<size_type>(sizeof(TStoredIntegral) * CHAR_BIT);
+            const auto bitsNeeded
+                = TLinearizeArrayDimsFunctor{}.size(Base::extents()) * VHBits::value() * flatFieldCount<TRecordDim>;
+            return roundUpToMultiple(bitsNeeded, bitsPerStoredIntegral) / CHAR_BIT;
+        }
+
+        template<std::size_t... RecordCoords, typename Blobs>
+        LLAMA_FN_HOST_ACC_INLINE constexpr auto compute(
+            typename Base::ArrayIndex ai,
+            RecordCoord<RecordCoords...>,
+            Blobs& blobs) const
+        {
+            constexpr auto flatFieldIndex = static_cast<size_type>(Flattener::template flatIndex<RecordCoords...>);
+            const auto bitOffset
+                = ((TLinearizeArrayDimsFunctor{}(ai, Base::extents()) * flatFieldCount<TRecordDim>) +flatFieldIndex)
+                * VHBits::value();
+
+            using QualifiedStoredIntegral = CopyConst<Blobs, TStoredIntegral>;
+            using DstType = GetType<TRecordDim, RecordCoord<RecordCoords...>>;
+            LLAMA_BEGIN_SUPPRESS_HOST_DEVICE_WARNING
+            return internal::BitPackedIntRef<DstType, QualifiedStoredIntegral, VHBits, size_type, SignBit>{
+                reinterpret_cast<QualifiedStoredIntegral*>(&blobs[0][0]),
+                bitOffset,
+                static_cast<const VHBits&>(*this)};
+            LLAMA_END_SUPPRESS_HOST_DEVICE_WARNING
+        }
+    };
+
+    /// Binds parameters to a \ref BitPackedIntAoS mapping except for array and record dimension, producing a quoted
+    /// meta function accepting the latter two. Useful to to prepare this mapping for a meta mapping.
+    template<
+        typename Bits = void,
+        SignBit SignBit = SignBit::Keep,
+        typename LinearizeArrayDimsFunctor = mapping::LinearizeArrayDimsCpp,
+        template<typename> typename FlattenRecordDim = FlattenRecordDimInOrder,
+        typename StoredIntegral = void>
+    struct BindBitPackedIntAoS
+    {
+        template<typename ArrayExtents, typename RecordDim>
+        using fn = BitPackedIntAoS<
+            ArrayExtents,
+            RecordDim,
+            std::conditional_t<!std::is_void_v<Bits>, Bits, typename ArrayExtents::value_type>,
+            SignBit,
+            LinearizeArrayDimsFunctor,
+            FlattenRecordDim,
+            std::conditional_t<
+                !std::is_void_v<StoredIntegral>,
+                StoredIntegral,
+                internal::StoredUnsignedFor<RecordDim>>>;
+    };
+
+    template<typename Mapping>
+    inline constexpr bool isBitPackedIntAoS = false;
+
+    template<
+        typename ArrayExtents,
+        typename RecordDim,
+        typename Bits,
+        SignBit SignBit,
+        typename LinearizeArrayDimsFunctor,
+        template<typename>
+        typename FlattenRecordDim,
+        typename StoredIntegral>
+    inline constexpr bool isBitPackedIntAoS<BitPackedIntAoS<
+        ArrayExtents,
+        RecordDim,
+        Bits,
+        SignBit,
+        LinearizeArrayDimsFunctor,
+        FlattenRecordDim,
+        StoredIntegral>> = true;
 } // namespace llama::mapping
 // ==
-// == ./mapping/BitPackedIntSoA.hpp ==
+// == ./mapping/BitPackedInt.hpp ==
 // ============================================================================
 
 // ============================================================================
@@ -8887,7 +9025,7 @@ namespace llama::mapping
 
 // #pragma once
 // #include "../ProxyRefOpMixin.hpp"    // amalgamate: file already expanded
-// #include "BitPackedIntSoA.hpp"    // amalgamate: file already expanded
+// #include "BitPackedInt.hpp"    // amalgamate: file already expanded
 // #include "Common.hpp"    // amalgamate: file already expanded
 
 // #include <algorithm>    // amalgamate: file already included
@@ -10501,7 +10639,7 @@ namespace llama::mapping
 // #include "mapping/AoS.hpp"    // amalgamate: file already expanded
 // #include "mapping/AoSoA.hpp"    // amalgamate: file already expanded
 // #include "mapping/BitPackedFloatSoA.hpp"    // amalgamate: file already expanded
-// #include "mapping/BitPackedIntSoA.hpp"    // amalgamate: file already expanded
+// #include "mapping/BitPackedInt.hpp"    // amalgamate: file already expanded
 // #include "mapping/Bytesplit.hpp"    // amalgamate: file already expanded
 // #include "mapping/Byteswap.hpp"    // amalgamate: file already expanded
 // #include "mapping/ChangeType.hpp"    // amalgamate: file already expanded
