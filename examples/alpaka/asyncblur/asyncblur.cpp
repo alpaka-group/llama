ef3267c48a27d333d6b067916913a3c9a800a32e
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2023-11-21 16:58:35 +0100

    Move alpakaHelpers into asyncblur example
    It was the only place where it was still used.

--- examples/alpaka/asyncblur/asyncblur.cpp
+++ examples/alpaka/asyncblur/asyncblur.cpp
@@ -30,6 +30,39 @@ constexpr auto elemsPerBlock = 16; /// number of elements per direction(!) every
 
 using FP = float;
 
+constexpr auto threadElemDistMinElem = 2;
+
+/** Returns a good guess for an optimal number of threads and elements in a
+ *  block based on the total number of elements in the block.
+ */
+template<typename T_Acc, std::size_t BlockSize, std::size_t HardwareThreads>
+struct ThreadsElemsDistribution
+{
+    /// number of elements per thread
+    static constexpr std::size_t elemCount = BlockSize;
+    /// number of threads per block
+    static constexpr std::size_t threadCount = 1u;
+};
+
+#ifdef ALPAKA_ACC_GPU_CUDA_ENABLED
+template<std::size_t BlockSize, std::size_t HardwareThreads, typename Dim, typename Size>
+struct ThreadsElemsDistribution<alpaka::AccGpuCudaRt<Dim, Size>, BlockSize, HardwareThreads>
+{
+    static constexpr std::size_t elemCount = threadElemDistMinElem;
+    static constexpr std::size_t threadCount = BlockSize / threadElemDistMinElem;
+};
+#endif
+
+#ifdef ALPAKA_ACC_CPU_B_SEQ_T_OMP2_ENABLED
+template<std::size_t BlockSize, std::size_t HardwareThreads, typename Dim, typename Size>
+struct ThreadsElemsDistribution<alpaka::AccCpuOmp2Threads<Dim, Size>, BlockSize, HardwareThreads>
+{
+    static constexpr std::size_t elemCount = (BlockSize + HardwareThreads - 1u) / HardwareThreads;
+    static constexpr std::size_t threadCount = HardwareThreads;
+};
+#endif
+
+
 // clang-format off
 namespace tag
 {
@@ -157,7 +190,7 @@ try
     int bufferY = defaultImgY + 2 * kernelSize;
 
     constexpr int hardwareThreads = 2; // relevant for OpenMP2Threads
-    using Distribution = common::ThreadsElemsDistribution<Acc, elemsPerBlock, hardwareThreads>;
+    using Distribution = ThreadsElemsDistribution<Acc, elemsPerBlock, hardwareThreads>;
     constexpr int elemCount = Distribution::elemCount;
     constexpr int threadCount = Distribution::threadCount;
 
