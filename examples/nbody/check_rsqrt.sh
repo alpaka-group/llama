66076c54e4cd9e18892d6154e67397ac405c6747
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2023-11-27 18:48:51 +0100

    Add a script to check for the presence of rsqrt

---
+++ examples/nbody/check_rsqrt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# run this on the generated n-body executable to see where rsqrt is generated
+objdump -SC $1 | awk '
+/^000.+/ {
+  if ($0 ~ /update/) {
+    f = $0;
+  } else {
+    f = ""
+  }
+}
+
+/r?sqrt/ {
+  if (f != "") {
+    if ($0 ~ "rsqrt")
+      printf "\033[32m"
+    else
+      printf "\033[31m"
+    print substr(f, 0, 80), "\n", $0
+  }
+}
+'
