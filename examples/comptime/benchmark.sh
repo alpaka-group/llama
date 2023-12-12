bbd2726f22cac8dde6f4b7c68d7d8e7e9058c3b1
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2023-12-12 16:10:35 +0100

    Repeat compile-time benchmark 20 times

--- examples/comptime/benchmark.sh
+++ examples/comptime/benchmark.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # the record dim has 1095 entries
+repetitions=20
 for i in {0..1000..20}; do
   cmake -DLLAMA_COMPTIME_RECORD_DIM_SIZE=$i .. > /dev/null 2>&1
-  s=$(\time -f "%e" make llama-comptime 2>&1 > /dev/null)
-  echo $i $s
+  for ((j=0; j < $repetitions; j++)); do
+    make clean
+    s=$(\time -f "%e" make llama-comptime 2>&1 > /dev/null)
+    echo $i $s
+  done
 done
