2a6625265fc96e05ac737b0a6ce05240db5f2829
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2023-12-13 14:35:39 +0100

    Improve synthetic comptime script

--- examples/comptime/benchmark.sh
+++ examples/comptime/benchmark.sh
@@ -6,7 +6,7 @@ for i in {0..1000..20}; do
   cmake -DLLAMA_COMPTIME_RECORD_DIM_SIZE=$i .. > /dev/null 2>&1
   for ((j=0; j < $repetitions; j++)); do
     make clean
-    s=$(\time -f "%e" make llama-comptime 2>&1 > /dev/null)
+    s=$( { TIMEFORMAT=%R; time make -j1 llama-comptime > /dev/null 2>&1 ; unset TIMEFORMAT; } 2>&1 )
     echo $i $s
   done
 done
